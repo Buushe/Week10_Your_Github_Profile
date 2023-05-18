35da396c52d68a7770f424a2a8b31eead95d85b3
Abdirahman Mohamed (Buushe) <79105703+Buushe@users.noreply.github.com>
2023-05-18 16:55:24 +0600

    done-my-profile

---
+++ src/components/repos/repositoriesCard.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function repositoriesCard(props) {
+
+   
+
+  return (
+    <div>
+        
+    </div>
+  )
+}
+
+export default repositoriesCard;
---
+++ src/components/repos/repositoriesList.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function repositoriesList(props) {
+
+   
+
+  return (
+    <div>
+        
+    </div>
+  )
+}
+
+export default repositoriesList;
